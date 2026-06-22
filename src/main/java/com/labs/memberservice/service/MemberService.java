cbf8a3aeb34364a85bc5fed2324d5f2078171db6
misung <crisine@knou.ac.kr>
2026-06-22 23:24:29 +0900

    feat:会員サービスの実装

---
+++ src/main/java/com/labs/memberservice/service/MemberService.java
@@ -0,0 +1,37 @@
+package com.labs.memberservice.service;
+
+import com.labs.memberservice.domain.Member;
+import com.labs.memberservice.repository.MemberRepository;
+import com.labs.memberservice.repository.MemoryMemberRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public class MemberService {
+
+    private final MemberRepository memberRepository = new MemoryMemberRepository();
+
+    // サインイン
+    public Long join(Member member) {
+        validateDuplicateMember(member);
+
+        memberRepository.save(member);
+        return member.getId();
+    }
+
+    private void validateDuplicateMember(Member member) {
+        memberRepository.findByName(member.getName())
+                .ifPresent(m -> {
+                    throw new IllegalStateException("Member already exists");
+                });
+    }
+
+    // 全会員の取得
+    public List<Member> findMembers() {
+        return memberRepository.findAll();
+    }
+
+    public Optional<Member> findOne(Long memberId) {
+        return memberRepository.findById(memberId);
+    }
+}
