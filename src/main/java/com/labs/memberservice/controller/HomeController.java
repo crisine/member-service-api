f3308525d2952e5ce51c9bc06058b8dbd1299d50
misung <crisine@knou.ac.kr>
2026-06-26 23:58:08 +0900

    fix:ウェブサーバー起動のため、gradleファイル修正æ¡

--- src/main/java/com/labs/memberservice/controller/HomeController.java
+++ src/main/java/com/labs/memberservice/controller/HomeController.java
@@ -1,4 +1,13 @@
 package com.labs.memberservice.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class HomeController {
+
+    @GetMapping("/")
+    public String home() {
+        return "home";
+    }
 }
