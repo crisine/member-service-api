a054aab175cacd05a1c965c1054db76657ca7b41
misung <crisine@knou.ac.kr>
2026-06-30 22:00:31 +0900

    refactor: メモリベースのデータ保存からH2データベース(JDBC)へ移行

--- src/main/java/com/labs/memberservice/SpringConfig.java
+++ src/main/java/com/labs/memberservice/SpringConfig.java
@@ -1,13 +1,24 @@
 package com.labs.memberservice;
 
+import com.labs.memberservice.repository.JdbcMemberRepository;
 import com.labs.memberservice.repository.MemberRepository;
 import com.labs.memberservice.repository.MemoryMemberRepository;
 import com.labs.memberservice.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import javax.sql.DataSource;
+
 @Configuration
 public class SpringConfig {
+
+    private DataSource dataSource;
+
+    @Autowired
+    SpringConfig(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
 
     @Bean
     public MemberService memberService() {
@@ -16,6 +27,7 @@ public class SpringConfig {
 
     @Bean
     public MemberRepository memberRepository() {
-        return new MemoryMemberRepository();
+        // return new MemoryMemberRepository();
+        return new JdbcMemberRepository(dataSource);
     }
 }
